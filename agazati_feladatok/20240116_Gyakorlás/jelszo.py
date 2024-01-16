30f55635e2297d052625694667c629fe42e23b74
lajosbarnabas0 <156697912+lajosbarnabas0@users.noreply.github.com>
2024-01-16 21:04:06 +0100

    Add files via upload

---
+++ agazati_feladatok/20240116_Gyakorlás/jelszo.py
@@ -0,0 +1,8 @@
+user = ''
+pwd = ''
+while user != 'bori99' and pwd != 'Szivecske<3':
+    user = input('Adja meg a felhasználónevet: ')
+    pwd = input('Adja meg a jelszavát: ')
+    if user != 'bori99' and pwd != 'Szivecske<3':
+        print('Belépés megtagadva.')
+print('Belépés engedélyezve.')
