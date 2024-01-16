eb65fa21f92cc86199e2eb95452022070e0c3eec
lajosbarnabas0 <156697912+lajosbarnabas0@users.noreply.github.com>
2024-01-16 21:04:36 +0100

    Delete 20240116_Gyakorlás directory

--- 20240116_Gyakorlás/jelszo.py
+++
@@ -1,8 +0,0 @@
-user = ''
-pwd = ''
-while user != 'bori99' and pwd != 'Szivecske<3':
-    user = input('Adja meg a felhasználónevet: ')
-    pwd = input('Adja meg a jelszavát: ')
-    if user != 'bori99' and pwd != 'Szivecske<3':
-        print('Belépés megtagadva.')
-print('Belépés engedélyezve.')
--- 20240116_Gyakorlás/szavak.py
+++
@@ -1,8 +0,0 @@
-szo1 = input('Adj meg egy szót: ')
-szo2 = input('Adj meg egy másik szót: ')
-if len(szo1) > len(szo2):
-    print(f'{szo1} hosszabb, mint {szo2}')
-elif len(szo1) < (szo2):
-    print(f'{szo1} rövidebb, mint {szo2}')
-else:
-    print(f'A két szó ugyanolyan hosszú')
